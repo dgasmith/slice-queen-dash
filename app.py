e6701b109c2a4172bfe1ea872fdae9c8bb8048e2
Daniel G. A. Smith <dgasmith@vt.edu>
2020-02-06 09:59:19 -0500

    Grabs the port env var

--- app.py
+++ app.py
@@ -211,4 +211,5 @@ def display_page(pathname):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run_server(debug=True, port=port)
